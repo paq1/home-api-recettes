83b264262533c15b3b665d7a60a74f83368f8a5a
Pierre Paquin <p.paquin1016@laposte.net>
2024-05-18 17:29:03 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HOME RECETTE API
+
+## run
+
+```shell
+python .\app.py
+```
+
+## install
+```shell
+pip install -r requirements.txt
+```
+
+## freeze lib
+
+```shell
+python -m pip freeze > "requirements.txt"
+```
--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/recette/1')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return {'data': {
+        'attributes': {
+            'name': 'chantilly',
+            'ingredients': [
+                'creme',
+                'sucre',
+                'arôme vanille'
+            ],
+            'steps': [
+                'xxxx',
+                'xxxx'
+            ]
+        }
+    }}
+
 
 if __name__ == '__main__':
     app.run()
